b3aaa0c00c502ca084235b07a81ef66dcf5cfffe
Ademir Sousa Lopes <ademir.sousa@esx.com.br>
2023-11-21 21:00:34 -0300

    Abre modal com alguns detalhes do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" data-id="${pokemon.number}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -27,6 +27,14 @@ function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
         pokemonList.innerHTML += newHtml
+
+        const pokemonItems = document.querySelectorAll('.pokemon');
+        pokemonItems.forEach((item) => {
+            const pokemonId = item.getAttribute('data-id');
+            item.addEventListener('click', () => {
+                loadPokemonDetails(pokemonId);
+            })
+        })
     })
 }
 
@@ -45,3 +53,27 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function loadPokemonDetails (pokemonId) {
+    pokeApi.getPokemon(pokemonId).then((pokemon) => {
+        const pokemonDetail = document.getElementById('pokemon-detail');
+        const newHtml = `
+        <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <h3>${pokemon.name}</h3>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        `;
+        pokemonDetail.innerHTML = newHtml
+    })
+
+    abreModal()
+}
+
+function abreModal() {
+    $("#myModal").modal({
+      show: true
+    });
+  }
